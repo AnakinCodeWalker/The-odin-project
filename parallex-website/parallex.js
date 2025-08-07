baac9749ac1de2d874b52986eb3ed759faf45ed7
AnakinCodeWalker <parag3111ja@gmail.com>
2025-08-08 01:20:23 +0530

    Added parallex effect files

---
+++ parallex-website/parallex.js
@@ -0,0 +1,12 @@
+// const mountain = document.querySelector(".mountain");
+// const balloon = document.querySelector(".ballon");
+// const heading = document.getElementById("heading");
+
+// window.addEventListener("scroll", () => {
+//     const scrollY = window.scrollY;
+
+//     // move at different speeds to create depth
+//     mountain.style.transform = `translateY(${scrollY * 0.2}px)`;
+//     balloon.style.transform = `translateY(${scrollY * 0.5}px)`;
+//     heading.style.transform = `translateY(${scrollY * 0.3}px)`;
+// });
