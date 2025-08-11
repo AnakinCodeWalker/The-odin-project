173963cf23dfb60301e1801b7890acdfbb773c1d
AnakinCodeWalker <parag3111ja@gmail.com>
2025-08-11 21:02:00 +0530

    updating prevoius works

--- todo app/todo.js
+++ todo app/todo.js
@@ -1,19 +1,1 @@
-const addBtn = document.getElementById('add-btn');
-const todoInput = document.getElementById('todo-input');
-const todoItemsContainer = document.getElementById('todo-items');
 
-addBtn.addEventListener('click', () => {
-    const value = todoInput.value;
-    const listItem = document.createElement('li');
-    listItem.innerText = value;
-
-    const deletebtn = document.createElement('button');
-    deletebtn.innerText = 'x';
-    listItem.appendChild(deletebtn);
-    todoItemsContainer.appendChild(listItem);
-
-    todoInput.value = "";
-    deletebtn.addEventListener('click', () => {
-        listItem.remove();
-    });
-});
